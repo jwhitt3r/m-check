a9a558e40db2bfce07e281952fa36f703a6877b1
Jack Whitter-Jones <jackwhitterjones@gmail.com>
2021-02-11 16:01:30 +0000

    Added URLCheck Testing
    Added Unit Test for URLCheck

---
+++ internal/urlcheck/urlcheck_test.go
@@ -0,0 +1,38 @@
+package urlcheck
+
+import (
+	"net/http"
+	"testing"
+)
+
+const success = "\u2713"
+const failure = "\u2717"
+
+func TestURLCheck(t *testing.T) {
+	tt := []struct {
+		url        string
+		statusCode int
+	}{
+		{"http://github.com/jwhitt3r/", http.StatusOK},
+		{"http://github.com/jwhitt3rasdasdasd3/", http.StatusNotFound},
+	}
+	t.Log("Given the need to fetch a URL")
+	{
+		for testID, test := range tt {
+			t.Logf("Test %d:\tWhen checking %q for a status code %d", testID, test.url, test.statusCode)
+			{
+				resp, err := http.Get(test.url)
+				if err != nil {
+					t.Fatalf("\t%s\tTest %d: Should be able to make a Get call %v", failure, testID, err)
+				}
+				t.Logf("\t%s\tTest %d: Should be able to make a Get call.", success, testID)
+
+				if resp.StatusCode == test.statusCode {
+					t.Logf("\t%s\tTest %d: Should receive a %d status code.", success, testID, test.statusCode)
+				} else {
+					t.Errorf("\t%s\tTest %d: Should receive a %d status code : %v", failure, testID, test.statusCode, resp.StatusCode)
+				}
+			}
+		}
+	}
+}
